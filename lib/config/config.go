ba50cd3831879a8df6f6c202f24785bbec71147b
shafnybuilds <shafnymhd@proton.me>
2025-04-01 17:09:17 +0530

    config.go updated

--- lib/config/config.go
+++ lib/config/config.go
@@ -1,12 +1,38 @@
 package config
 
-import "time"
+import (
+	"log"
+	"time"
 
-type config struct {
+	"github.com/spf13/viper"
+)
+
+type Config struct {
 	Environment         string        `mapstructure:"ENV"`
 	LogLevel            string        `mapstructure:"LOG_LEVEL"`
 	DBConnString        string        `mapstructure:"DB_CONN_STRING"`
 	HTTPServerAddress   string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	PASETOSecret        string        `mapstructure:"PASETO_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstucture:"ACCESS_TOKEN_DURATION"`
+}
+
+// Load and read the configs from file or environment variables
+func Load(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("development")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadConfig()
+	if err != nil {
+		log.Fatalf("Viper couldn't read in the config file. %v", err)
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("Viper couldn't unmarshal the configuration. %v", err)
+	}
+
+	return
 }
