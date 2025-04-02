e89f4078e1559e995657feaf7ab7a9eccdf4b3bf
shafnybuilds <shafnymhd@proton.me>
2025-04-02 12:47:30 +0530

    password checks updated

--- lib/password/password.go
+++ lib/password/password.go
@@ -1,6 +1,34 @@
 package password
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 var ErrTooShort = errors.New("the given password is too short")
 
+func Hash(password string) (string error) {
+	if len(password) < 5 {
+		return "", ErrTooShort
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", fmt.Errorf("could not hash user password! %v", err)
+	}
+	return string(hash), nil
+
+	func Validate(hashedPassword string, password string) error {
+		if len(password) < 5 {
+			return ErrTooShort
+		}
+		err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+		if err != nil {
+			return fmt.Errorf("error while comparing the current and hashed password! %v", err)
+		}
+		return nil
+	}
+}
---
+++ lib/password/password_test.go
@@ -0,0 +1,56 @@
+package password
+
+import (
+	"testing"
+
+	"github.com/adykaaa/online-notes/lib/random"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHashUserPassword(t *testing.T) {
+	t.Run("password hashing OK", func(t *testing.T) {
+		hpw, err := Hash(random.NewString(10))
+		require.NoError(t, err)
+		require.NotEmpty(t, hpw)
+	})
+
+	t.Run("same hash for different password", func(t *testing.T) {
+		pw := random.NewString(20)
+		hpw1, err := Hash(pw)
+		require.NoError(t, err)
+		hpw2, err := Hash(pw)
+		require.NoError(t, err)
+		require.NotEqual(t, hpw1, hpw2)
+	})
+	t.Run("fails if too short", func(t *testing.T) {
+		hpw, err := Hash(random.NewString(4))
+		require.Error(t, err)
+		require.Empty(t, hpw)
+	})
+	t.Run("fails if empty", func(t *testing.T) {
+		hpw, err := Hash("")
+		require.Error(t, err)
+		require.Empty(t, hpw)
+	})
+	t.Run("fails if too long", func(t *testing.T) {
+		hpw, err := Hash(random.NewString(100))
+		require.Error(t, err)
+		require.Empty(t, hpw)
+	})
+
+}
+
+func TestValidate(t *testing.T) {
+	const pw1 = "abc123!"
+	const pw2 = "abc321!"
+	t.Run("password validation OK", func(t *testing.T) {
+		hpw, _ := Hash(pw1)
+		err := Validate(hpw, pw1)
+		require.NoError(t, err)
+	})
+	t.Run("fails with not same hash", func(t *testing.T) {
+		hpw, _ := Hash(pw1)
+		err := Validate(hpw, pw2)
+		require.Error(t, err)
+	})
+}
